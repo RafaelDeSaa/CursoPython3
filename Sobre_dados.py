25941f7aa6658fb39d4b10c2440f3f2187a44d5d
Rafael de Sá <rafaelmoreiraaraujodesa@gmail.com>
2024-02-16 16:39:40 -0300

    Sobre_dados

---
+++ Sobre_dados.py
@@ -0,0 +1,12 @@
+nome = 'Rafael'
+sobrenome = 'Moreira Araújo de Sá'
+idade = 29
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = float(1.76)
+print ('Nome:', nome)
+print ('Sobrenome:', sobrenome)
+print ('Idade:', idade)
+print ('Ano de nascimento:', ano_nascimento)
+print ('É maior de idade?', maior_de_idade)
+print ('Altura em metros:', altura_metros)
