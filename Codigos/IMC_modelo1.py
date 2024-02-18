8b09984b147ddc45812432bd58c6419f543ac7ba
Rafael de Sá <rafaelmoreiraaraujodesa@gmail.com>
2024-02-18 14:36:26 -0300

    IMC modelo 1 e 2

---
+++ Codigos/IMC_modelo1.py
@@ -0,0 +1,7 @@
+nome = 'Rafael Moreira'
+altura = 1.76
+peso = 118
+imc = peso / (altura * altura)
+print (nome,'tem',altura, 'de altura,')
+print ('pesa:', peso,'quilos', 'e seu IMC é:')
+print (imc)
